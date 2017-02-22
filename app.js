ec4526d18dbc0fd24b2b3197729f560a9e984d80
Kyle Bradford <kfoxb@kfoxb.com>
2017-02-22 14:54:36 -0800

    Complete react components solo sprint

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-// TODO
+class GroceryListItem extends React.Component {
+  constructor (props) {
+    super (props);
+    this.state = {
+      done: false
+    };
+  }
+
+  onListItemClick () {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render () {
+    var style = {
+      'font-weight': this.state.done ? 'bold' : 'normal'
+    };
+    return (
+        <li style={style} onMouseEnter={this.onListItemClick.bind(this)}>{this.props.groceries}</li>
+    );
+  }
+
+}
+
+var App = () => (
+
+  <div>
+    <h2>Grocery List</h2>
+    <GroceryListItem groceries={'Lettuce'}/>
+    <GroceryListItem groceries={'Tomatoes'}/>
+    <GroceryListItem groceries={'Bananas'}/>
+  </div>
+
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
